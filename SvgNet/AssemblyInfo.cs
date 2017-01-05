e3d426c988db088f47cce768a042dc5cda2eae5b
Rafael Teixeira <monoman@gmail.com>
2017-01-05 12:58:03 -0200

    Moving to VS2015 Moving some files to C# 6, with cleanup

--- SvgNet/AssemblyInfo.cs
+++ SvgNet/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿/*
 	Copyright © 2003 RiskCare Ltd. All rights reserved.
 	Copyright © 2010 SvgNet & SvgGdi Bridge Project. All rights reserved.
-	Copyright © 2015 Rafael Teixeira, Mojmír Němeček, Benjamin Peterson and Other Contributors
+	Copyright © 2015, 2017 Rafael Teixeira, Mojmír Němeček, Benjamin Peterson and Other Contributors
 
 	Original source code licensed with BSD-2-Clause spirit, treat it thus, see accompanied LICENSE for more
 */
@@ -15,13 +15,13 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RiskCare Ltd, SvgNet and SvgGdi Bridge Project, Rafael Teixeira, Mojmír Němeček, Benjamin Peterson")]
 [assembly: AssemblyProduct("SvgNet")]
-[assembly: AssemblyCopyright("Copyright 2003, 2010, 2015 RiskCare Ltd, SvgNet and SvgGdi Bridge Project, Rafael Teixeira, Mojmír Němeček, Benjamin Peterson")]
+[assembly: AssemblyCopyright("Copyright 2003, 2010, 2015, 2017 RiskCare Ltd, SvgNet and SvgGdi Bridge Project, Rafael Teixeira, Mojmír Němeček, Benjamin Peterson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
--- SvgNet/SvgElementFactory.cs
+++ SvgNet/SvgElementFactory.cs
@@ -1,285 +1,280 @@
 /*
-	Copyright © 2003 RiskCare Ltd. All rights reserved.
-	Copyright © 2010 SvgNet & SvgGdi Bridge Project. All rights reserved.
-	Copyright © 2015 Rafael Teixeira, Mojmír Němeček, Benjamin Peterson and Other Contributors
+    Copyright © 2003 RiskCare Ltd. All rights reserved.
+    Copyright © 2010 SvgNet & SvgGdi Bridge Project. All rights reserved.
+    Copyright © 2015, 2017 Rafael Teixeira, Mojmír Němeček, Benjamin Peterson and Other Contributors
 
-	Original source code licensed with BSD-2-Clause spirit, treat it thus, see accompanied LICENSE for more
+    Original source code licensed with BSD-2-Clause spirit, treat it thus, see accompanied LICENSE for more
 */
 
-
+using SvgNet.SvgElements;
 using System;
 using System.Collections;
-using System.IO;
-using System.Xml;
 using System.Reflection;
-using SvgNet.SvgElements;
+using System.Xml;
 
 namespace SvgNet
 {
-	/// <summary>
-	/// Static methods to produce/write/copy Svg documents reside in this class.
-	/// </summary>
-	public class SvgFactory
-	{
-		private SvgFactory()
-		{
-		}
+    /// <summary>
+    /// Static methods to produce/write/copy Svg documents reside in this class.
+    /// </summary>
+    public class SvgFactory
+    {
+        /// <summary>
+        /// Create a complete deep copy of the given tree of <c>SvgElement</c> objects.
+        /// A new set of elements is created, and if the attributes are cloneable they are deep-copied too.
+        /// Since strings and all SvgType classes are cloneable, the new tree is independant of the old.
+        /// </summary>
+        /// <param name="el"></param>
+        /// <returns></returns>
+        public static SvgElement CloneElement(SvgElement el)
+        {
+            var clone = (SvgElement)el.GetType().GetConstructor(new System.Type[0]).Invoke(new object[0]);
 
-		private static Hashtable _elementNameDictionary;
+            foreach (string key in el.Attributes.Keys)
+            {
+                var o = el[key];
+                if (typeof(ICloneable).IsInstanceOfType(o))
+                    clone[key] = ((ICloneable)o).Clone();
+                else
+                    clone[key] = o;
+            }
 
-		/// <summary>
-		/// Given an xml document and (optionally) a particular element to start from, read the xml nodes and construct
-		/// a tree of <see cref="SvgElement"/> objects.  Xml tags that do not correspond to a particular class will be
-		/// represented by an <see cref="SvgGenericElement"/>.  This means that literally any XML input can be read in
-		/// and written out, even if it has nothing to do with Svg.  More usefully, it means that new and unsupported tags
-		/// and attributes will be represented in the <c>SvgElement</c> tree and written out correctly even if SvgNet does
-		/// not understand them.
-		/// </summary>
-		/// <param name="doc"></param>
-		/// <param name="el"></param>
-		/// <returns></returns>
-		public static SvgElement LoadFromXML(XmlDocument doc, XmlElement el)
-		{
-			if (el == null)
-			{
-				foreach(XmlNode noddo in doc.ChildNodes)
-				{
-					if (noddo.GetType() == typeof(XmlElement))
-					{
-						el = (XmlElement)noddo;
-						break;
-					}
-				}
-			}
+            foreach (SvgElement ch in el.Children)
+            {
+                clone.AddChild(CloneElement(ch));
+            }
 
-			if (el == null)
-				return null;
+            return clone;
+        }
 
-			if (_elementNameDictionary == null)
-			{
-				BuildElementNameDictionary();
-			}
+        /// <summary>
+        /// Helper function to compress long xml attributes into entities.
+        /// <para>
+        /// This would work on any XML, it is not SVG specific, so it should eventually be in some 'xml tools' class.
+        /// </para>
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="el"></param>
+        /// <returns>A string of entities which can be inserted into the DOCTYPE when the document is written.</returns>
+        public static string CompressXML(XmlDocument doc, XmlElement el)
+        {
+            var entities = new Hashtable();
+            var singletons = new Hashtable();
 
-			Type t = (Type)_elementNameDictionary[el.Name];
+            int idx = 0;
+            RecCompXML(entities, singletons, doc, el, ref idx);
 
-			SvgElement e = (SvgElement)t.GetConstructor(new System.Type[0]).Invoke(new object[0]);
+            // "Uncompress" attribute values with only one reference as it would actually
+            // make the resulting XML bigger
+            foreach (DictionaryEntry pair in singletons)
+            {
+                var attributeValue = (string)pair.Key;
+                var singleton = (EntitySingleton)pair.Value;
 
-			RecLoadFromXML(e, doc, el);
+                // This is an inverse behavior of what RecCompXML did
+                singleton.Element.RemoveAttribute(singleton.AttributeName);
+                var attr = doc.CreateAttribute(singleton.AttributeName);
+                attr.Value = attributeValue;
+                singleton.Element.SetAttributeNode(attr);
 
-			return e;
-		}
+                entities.Remove(attributeValue);
+            }
 
-		/// <summary>
-		/// Used by LoadFromXML
-		/// </summary>
-		/// <param name="e"></param>
-		/// <param name="doc"></param>
-		/// <param name="el"></param>
-		private static void RecLoadFromXML(SvgElement e, XmlDocument doc, XmlElement el)
-		{
-			e.ReadXmlElement(doc, el);
+            var ents = new System.Text.StringBuilder();
 
-			foreach(XmlNode noddo in el.ChildNodes)
-			{
-				if (noddo.GetType() == typeof(XmlElement))
-				{
-					XmlElement childXml = (XmlElement)noddo;
+            if (entities.Count > 0)
+            {
+                ents.Append("\n");
 
-					Type t = (Type)_elementNameDictionary[childXml.Name];
+                foreach (string key in entities.Keys)
+                {
+                    ents.Append("<!ENTITY ");
+                    ents.Append(entities[key]);
+                    ents.Append(" '");
+                    ents.Append(key.Replace("%", "&#37;"));//I guess we need to escape fully, but surely no other special char can appear?
+                    ents.Append("'>");
+                }
 
-					SvgElement childSvg=null;
+                ents.Append("\n");
+            }
 
-					if (t == null)
-					{
-						childSvg = new SvgGenericElement(childXml.Name);
-					}
-					else
-					{
-						childSvg = (SvgElement)t.GetConstructor(new System.Type[0]).Invoke(new object[0]);
-					}
+            return ents.ToString();
+        }
 
-					e.AddChild(childSvg);
+        /// <summary>
+        /// Given an xml document and (optionally) a particular element to start from, read the xml nodes and construct
+        /// a tree of <see cref="SvgElement"/> objects.  Xml tags that do not correspond to a particular class will be
+        /// represented by an <see cref="SvgGenericElement"/>.  This means that literally any XML input can be read in
+        /// and written out, even if it has nothing to do with Svg.  More usefully, it means that new and unsupported tags
+        /// and attributes will be represented in the <c>SvgElement</c> tree and written out correctly even if SvgNet does
+        /// not understand them.
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="el"></param>
+        /// <returns></returns>
+        public static SvgElement LoadFromXML(XmlDocument doc, XmlElement el)
+        {
+            if (el == null)
+            {
+                foreach (XmlNode noddo in doc.ChildNodes)
+                {
+                    if (noddo.GetType() == typeof(XmlElement))
+                    {
+                        el = (XmlElement)noddo;
+                        break;
+                    }
+                }
+            }
 
-					RecLoadFromXML(childSvg, doc, childXml);
-				}
-				else if (noddo.GetType() == typeof(XmlText))
-				{
-					XmlText xt = (XmlText)noddo;
+            if (el == null)
+                return null;
 
-					TextNode tn = new TextNode(xt.InnerText);
+            if (_elementNameDictionary == null)
+            {
+                BuildElementNameDictionary();
+            }
 
-					e.AddChild(tn);
-				}
-			}
-		}
+            var t = (Type)_elementNameDictionary[el.Name];
 
-		/// <summary>
-		/// Used by LoadFromXML
-		/// </summary>
-		private static void BuildElementNameDictionary()
-		{
+            var e = (SvgElement)t.GetConstructor(new System.Type[0]).Invoke(new object[0]);
 
-			_elementNameDictionary = new Hashtable();
+            RecLoadFromXML(e, doc, el);
 
-			Assembly asm = Assembly.GetExecutingAssembly();
+            return e;
+        }
 
-			Type[] ta = asm.GetExportedTypes();
+        private static Hashtable _elementNameDictionary;
 
-			foreach(Type t in ta)
-			{
-				if (t.IsSubclassOf(typeof(SvgElement)))
-				{
-					SvgElement e = (SvgElement)t.GetConstructor(new System.Type[0]).Invoke(new object[0]);
+        private SvgFactory()
+        {
+        }
 
-					if (e.Name != "?" /* default name of abstract SvgElements */)
-					{
-						_elementNameDictionary[e.Name] = e.GetType();
-					}
-				}
-			}
-		
-		}
+        /// <summary>
+        /// Used by LoadFromXML
+        /// </summary>
+        private static void BuildElementNameDictionary()
+        {
+            _elementNameDictionary = new Hashtable();
 
-		/// <summary>
-		/// Helper function to compress long xml attributes into entities.
-		/// <para>
-		/// This would work on any XML, it is not SVG specific, so it should eventually be in some 'xml tools' class.
-		/// </para>
-		/// </summary>
-		/// <param name="doc"></param>
-		/// <param name="el"></param>
-		/// <returns>A string of entities which can be inserted into the DOCTYPE when the document is written.</returns>
-		public static string CompressXML(XmlDocument doc, XmlElement el)
-		{
-			Hashtable entities = new Hashtable();
-			Hashtable singletons = new Hashtable();
+            var asm = Assembly.GetExecutingAssembly();
 
-			int idx = 0;
-			RecCompXML(entities, singletons, doc, el, ref idx);
+            var ta = asm.GetExportedTypes();
 
-			// "Uncompress" attribute values with only one reference as it would actually
-			// make the resulting XML bigger
-			foreach(DictionaryEntry pair in singletons)
-			{
-				var attributeValue = (string)pair.Key;
-				var singleton = (EntitySingleton)pair.Value;
+            foreach (Type t in ta)
+            {
+                if (t.IsSubclassOf(typeof(SvgElement)))
+                {
+                    var e = (SvgElement)t.GetConstructor(new System.Type[0]).Invoke(new object[0]);
 
-				// This is an inverse behavior of what RecCompXML did
-				singleton.Element.RemoveAttribute(singleton.AttributeName);
-				XmlAttribute attr = doc.CreateAttribute(singleton.AttributeName);
-				attr.Value = attributeValue;
-				singleton.Element.SetAttributeNode(attr);
+                    if (e.Name != "?" /* default name of abstract SvgElements */)
+                    {
+                        _elementNameDictionary[e.Name] = e.GetType();
+                    }
+                }
+            }
+        }
 
-				entities.Remove(attributeValue);
-			}
+        /// <summary>
+        /// Used by CompressXML
+        /// </summary>
+        /// <param name="entities">Map of attribute to entity name</param>
+        /// <param name="singletons">Output: List of single use attributes to 'uncompress'</param>
+        /// <param name="doc"></param>
+        /// <param name="el"></param>
+        /// <param name="idx">Number that is incremented to provide new entity names</param>
+        private static void RecCompXML(Hashtable entities, Hashtable singletons, XmlDocument doc, XmlElement el, ref int idx)
+        {
+            var keys = new ArrayList();
 
-			var ents = new System.Text.StringBuilder();
+            foreach (XmlAttribute att in el.Attributes)
+            {
+                keys.Add(att.Name);
+            }
 
-			if (entities.Count > 0)
-			{
-				ents.Append("\n");
+            foreach (string s in keys)
+            {
+                var val = el.Attributes[s].Value;
 
-				foreach (string key in entities.Keys)
-				{
-					ents.Append("<!ENTITY ");
-					ents.Append(entities[key]);
-					ents.Append(" '");
-					ents.Append(key.Replace("%", "&#37;"));//I guess we need to escape fully, but surely no other special char can appear?
-					ents.Append("'>");
-				}
+                if (val.Length > 30)
+                {
+                    string entname;
 
-				ents.Append("\n");
-			}
+                    if (entities[val] == null)
+                    {
+                        idx += 1;
+                        entname = "E" + idx.ToString();
+                        entities[val] = entname;
 
-			return ents.ToString();
-		}
+                        singletons[val] = new EntitySingleton() { Element = el, AttributeName = s };
+                    }
+                    else
+                    {
+                        entname = (string)entities[val];
 
-		private struct EntitySingleton
-		{
-			public XmlElement Element;
-			public string AttributeName;
-		}
+                        singletons.Remove(val);
+                    }
 
-		/// <summary>
-		/// Used by CompressXML
-		/// </summary>
-		/// <param name="counts">Map of attribute to number of occurrences -- could be used to improve algorithm</param>
-		/// <param name="entities">Map of attribute to entity name</param>
-		/// <param name="doc"></param>
-		/// <param name="el"></param>
-		/// <param name="idx">Number that is incremented to provide new entity names</param>
-		private static void RecCompXML(Hashtable entities, Hashtable singletons, XmlDocument doc, XmlElement el, ref int idx)
-		{	
-			ArrayList keys = new ArrayList();
+                    var attr = doc.CreateAttribute(s);
+                    attr.AppendChild(doc.CreateEntityReference(entname));
+                    el.SetAttributeNode(attr);
+                }
+            }
 
-			foreach(XmlAttribute att in el.Attributes)
-			{
-				keys.Add(att.Name);
-			}
+            foreach (XmlNode ch in el.ChildNodes)
+            {
+                if (ch.GetType() == typeof(XmlElement))
+                    RecCompXML(entities, singletons, doc, (XmlElement)ch, ref idx);
+            }
+        }
 
-			foreach(string s in keys)
-			{
-				string val = el.Attributes[s].Value;
-	
-				if (val.Length > 30)
-				{
-					string entname;
+        /// <summary>
+        /// Used by LoadFromXML
+        /// </summary>
+        /// <param name="e"></param>
+        /// <param name="doc"></param>
+        /// <param name="el"></param>
+        private static void RecLoadFromXML(SvgElement e, XmlDocument doc, XmlElement el)
+        {
+            e.ReadXmlElement(doc, el);
 
-					if(entities[val] == null)
-					{
-						idx += 1;
-						entname = "E"+idx.ToString();
-						entities[val] = entname;
+            foreach (XmlNode noddo in el.ChildNodes)
+            {
+                if (noddo.GetType() == typeof(XmlElement))
+                {
+                    var childXml = (XmlElement)noddo;
 
-						singletons[val] = new EntitySingleton() { Element = el, AttributeName = s };
-					}
-					else
-					{
-						entname = (string)entities[val];
+                    var t = (Type)_elementNameDictionary[childXml.Name];
 
-						singletons.Remove(val);
-					}
+                    SvgElement childSvg = null;
 
-					XmlAttribute attr = doc.CreateAttribute(s);
-					attr.AppendChild(doc.CreateEntityReference(entname));
-					el.SetAttributeNode(attr);
-				}
+                    if (t == null)
+                    {
+                        childSvg = new SvgGenericElement(childXml.Name);
+                    }
+                    else
+                    {
+                        childSvg = (SvgElement)t.GetConstructor(new System.Type[0]).Invoke(new object[0]);
+                    }
 
-			}
+                    e.AddChild(childSvg);
 
-			foreach(XmlNode ch in el.ChildNodes)
-			{
-				if (ch.GetType() == typeof(XmlElement))
-					RecCompXML(entities, singletons, doc, (XmlElement)ch, ref idx);
-			}
-		}
-	
-		/// <summary>
-		/// Create a complete deep copy of the given tree of <c>SvgElement</c> objects.
-		/// A new set of elements is created, and if the attributes are cloneable they are deep-copied too.
-		/// Since strings and all SvgType classes are cloneable, the new tree is independant of the old.
-		/// </summary>
-		/// <param name="el"></param>
-		/// <returns></returns>
-		public static SvgElement CloneElement(SvgElement el)
-		{
-			SvgElement clone = (SvgElement)el.GetType().GetConstructor(new System.Type[0]).Invoke(new object[0]);
+                    RecLoadFromXML(childSvg, doc, childXml);
+                }
+                else if (noddo.GetType() == typeof(XmlText))
+                {
+                    var xt = (XmlText)noddo;
 
-			foreach(string key in el.Attributes.Keys)
-			{
-				object o = el[key];
-				if (typeof(ICloneable).IsInstanceOfType(o))
-					clone[key] = ((ICloneable)o).Clone();
-				else
-					clone[key] = o;
-			}
+                    var tn = new TextNode(xt.InnerText);
 
-			foreach(SvgElement ch in el.Children)
-			{
-				clone.AddChild(CloneElement(ch));
-			}
+                    e.AddChild(tn);
+                }
+            }
+        }
 
-			return clone;
-		}
-	}
+        private struct EntitySingleton
+        {
+            public string AttributeName;
+            public XmlElement Element;
+        }
+    }
 }
