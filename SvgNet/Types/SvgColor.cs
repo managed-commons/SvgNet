d9328ab60617094f438e9bd86982b5f3ed2dd14b
Rafael Teixeira <rteixeira@il2.io>
2022-11-09 22:33:54 -0300

    v3.3.1 - onboarding .NET 7.0

--- SvgNet/Types/SvgColor.cs
+++ SvgNet/Types/SvgColor.cs
@@ -6,6 +6,7 @@
     Original source code licensed with BSD-2-Clause spirit, treat it thus, see accompanied LICENSE for more
 */
 
+using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
 
 namespace SvgNet.Types;
@@ -14,7 +15,7 @@ namespace SvgNet.Types;
 /// the string it was initialized from, so that when a color specified as 'black' is written out,
 /// it will be written 'black' rather than '#000000'
 /// </summary>
-public class SvgColor : ICloneable {
+public partial class SvgColor : ICloneable {
     public SvgColor(string s) => FromString(s);
 
     public SvgColor(Color c) => Color = c;
@@ -31,32 +32,6 @@ public class SvgColor : ICloneable {
     public static implicit operator SvgColor(string s) => new(s);
 
     public object Clone() => new SvgColor(Color, _original_string);
-
-    private static readonly Regex rg = new("[rR][gG][bB]");
-
-    /// <summary>
-    /// As well as parsing the four types of CSS color descriptor (rgb, #xxxxxx, color name, and system color name),
-    /// the FromString of this type stores the original string
-    /// </summary>
-    /// <param name="s"></param>
-    public void FromString(string s) {
-        _original_string = s;
-
-        if (s.StartsWith("#", StringComparison.Ordinal)) {
-            FromHexString(s);
-            return;
-        }
-
-        if (rg.Match(s).Success) {
-            FromRGBString(s);
-            return;
-        }
-
-        Color = Color.FromName(s);
-
-        if (Color.A == 0)
-            throw new SvgException("Invalid SvgColor", s);
-    }
 
     /// <summary>
     /// If the SvgColor was constructed from a string, use that string; otherwise use rgb() form
@@ -79,48 +54,88 @@ public class SvgColor : ICloneable {
 
     private string _original_string;
 
-    private void FromHexString(string s) {
-        int r, g, b;
-        if (s.Length == 4) {
-            r = s.ParseHex(1, 1);
-            g = s.ParseHex(2, 1);
-            b = s.ParseHex(3, 1);
-            r += r * 16;
-            g += g * 16;
-            b += b * 16;
-            Color = Color.FromArgb(r, g, b);
-        } else if (s.Length == 7) {
-            r = s.ParseHex(1, 2);
-            g = s.ParseHex(3, 2);
-            b = s.ParseHex(5, 2);
-            Color = Color.FromArgb(r, g, b);
-        } else             throw new SvgException("Invalid SvgColor", s);
-    }
+    /// <summary>
+    /// As well as parsing the four types of CSS color descriptor (rgb, #xxxxxx, color name, and system color name),
+    /// the FromString of this type stores the original string
+    /// </summary>
+    /// <param name="s"></param>
+    public void FromString(string s) {
+        _original_string = s;
+        Color = TryParseHexString(s, out Color color) || TryParseRGB(s, out color) || TryParseFromName(s, out color)
+            ? color
+            : throw new SvgException("Invalid SvgColor", s);
 
-    private void FromRGBString(string s) {
-        int r, g, b;
-        var rg = new Regex(@"[rgbRGB ]+\( *(?<r>\d+)[, ]+(?<g>\d+)[, ]+(?<b>\d+) *\)");
-        Match m = rg.Match(s);
-        if (m.Success) {
-            r = int.Parse(m.Groups["r"].Captures[0].Value, CultureInfo.InvariantCulture);
-            g = int.Parse(m.Groups["g"].Captures[0].Value, CultureInfo.InvariantCulture);
-            b = int.Parse(m.Groups["b"].Captures[0].Value, CultureInfo.InvariantCulture);
-
-            Color = Color.FromArgb(r, g, b);
-            return;
+        static bool TryParseHexString(string s, out Color color) {
+            if (s.StartsWith("#", StringComparison.Ordinal)) {
+                int r, g, b;
+                if (s.Length == 4) {
+                    r = s.ParseHex(1, 1);
+                    g = s.ParseHex(2, 1);
+                    b = s.ParseHex(3, 1);
+                    r += r * 16;
+                    g += g * 16;
+                    b += b * 16;
+                    color = Color.FromArgb(r, g, b);
+                    return true;
+                }
+                if (s.Length == 7) {
+                    r = s.ParseHex(1, 2);
+                    g = s.ParseHex(3, 2);
+                    b = s.ParseHex(5, 2);
+                    color = Color.FromArgb(r, g, b);
+                    return true;
+                }
+            }
+            color = Color.Transparent;
+            return false;
         }
 
-        rg = new Regex(@"[rgbRGB ]+\( *(?<r>\d+)%[, ]+(?<g>\d+)%[, ]+(?<b>\d+)% *\)");
-        m = rg.Match(s);
-        if (m.Success) {
-            r = int.Parse(m.Groups["r"].Captures[0].Value, CultureInfo.InvariantCulture) * 255 / 100;
-            g = int.Parse(m.Groups["g"].Captures[0].Value, CultureInfo.InvariantCulture) * 255 / 100;
-            b = int.Parse(m.Groups["b"].Captures[0].Value, CultureInfo.InvariantCulture) * 255 / 100;
+        static bool TryParseRGB(string s, out Color color) {
+            if (RGB_PREFIX_REGEX.Match(s).Success)
+                return TryParseScaling(s, RGB_INTEGERS_REGEX, 100, out color) || TryParseScaling(s, RGB_PERCENTS_REGEX, 255, out color);
+            else {
+                color = Color.Transparent;
+                return false;
+            }
 
-            Color = Color.FromArgb(r, g, b);
-            return;
+            static bool TryParseScaling(string s, Regex regex, int scale, out Color color) {
+                Match m = regex.Match(s);
+                if (m.Success) {
+                    int r, g, b;
+                    r = Parse(m, "r") * scale / 100;
+                    g = Parse(m, "g") * scale / 100;
+                    b = Parse(m, "b") * scale / 100;
+                    color = Color.FromArgb(r, g, b);
+                    return true;
+
+                    static int Parse(Match m, string component) =>
+                        int.Parse(m.Groups[component].Captures[0].Value, CultureInfo.InvariantCulture);
+                }
+                color = Color.Transparent;
+                return false;
+            }
         }
 
-        throw new SvgException("Invalid SvgColor", s);
+        static bool TryParseFromName(string s, out Color color) {
+            color = Color.FromName(s);
+            return color.A != 0;
+        }
     }
+
+#if NET7_0_OR_GREATER
+    private static readonly Regex RGB_PREFIX_REGEX = RGB_PREFIX();
+    private static readonly Regex RGB_INTEGERS_REGEX = RGB_INTEGERS();
+    private static readonly Regex RGB_PERCENTS_REGEX = RGB_PERCENTS();
+
+    [GeneratedRegex("[rR][gG][bB]")]
+    private static partial Regex RGB_PREFIX();
+    [GeneratedRegex("[rgbRGB ]+\\( *(?<r>\\d+)[, ]+(?<g>\\d+)[, ]+(?<b>\\d+) *\\)")]
+    private static partial Regex RGB_INTEGERS();
+    [GeneratedRegex("[rgbRGB ]+\\( *(?<r>\\d+)%[, ]+(?<g>\\d+)%[, ]+(?<b>\\d+)% *\\)")]
+    private static partial Regex RGB_PERCENTS();
+#else
+    private static readonly Regex RGB_PREFIX_REGEX = new("[rR][gG][bB]");
+    private static readonly Regex RGB_INTEGERS_REGEX = new("[rgbRGB ]+\\( *(?<r>\\d+)[, ]+(?<g>\\d+)[, ]+(?<b>\\d+) *\\)");
+    private static readonly Regex RGB_PERCENTS_REGEX = new("[rgbRGB ]+\\( *(?<r>\\d+)%[, ]+(?<g>\\d+)%[, ]+(?<b>\\d+)% *\\)");
+#endif
 }
